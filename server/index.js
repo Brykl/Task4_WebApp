c6d7e9ce3e1d0cd8a743031bf9b5b015be832eed
Brykl <153744203+Brykl@users.noreply.github.com>
2024-04-27 21:37:16 +0500

    test server

--- server/index.js
+++ server/index.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const port = 3030
+
+app.use(express.json());
+app.use(cors());
 
 app.get('/', (req, res) => {
     res.send("Hello World");
+})
+
+app.post('/login', (req, res) => {
+    const userData = req.body;
+    res.status(201).json({ message: "Server works", userData });
 })
 
 app.listen(port, () => {
