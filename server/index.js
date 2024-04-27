e4e2a7ec624cd72f097063a3615e29a461cd6dd4
Brykl <153744203+Brykl@users.noreply.github.com>
2024-04-28 01:56:26 +0500

    Woked2

--- server/index.js
+++ server/index.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
-const cors = require('cors');
+// const cors = require('cors');
 const port = 3030;
 const EmployeeModel = require('./models/employee')
 
 app.use(express.json());
-app.use(cors());
+// app.use(cors());
 
 app.get('/', (req, res) => {
     res.send("Hello World");
